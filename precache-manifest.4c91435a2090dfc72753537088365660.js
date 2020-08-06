c297fa93e6fc08266b2dce412934e0a02bec4386
AmishaRathore <amisharathore3098@gmail.com>
2020-08-06 12:10:21 +0530

    Updates

---
+++ precache-manifest.4c91435a2090dfc72753537088365660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d1a78ab21ee4f52371059a9473492f",
+    "url": "/Screen-1-react/index.html"
+  },
+  {
+    "revision": "dc6d7300e60c258b9dce",
+    "url": "/Screen-1-react/static/css/2.12f1a8df.chunk.css"
+  },
+  {
+    "revision": "54a551498792d0ff9328",
+    "url": "/Screen-1-react/static/css/main.72f28aeb.chunk.css"
+  },
+  {
+    "revision": "dc6d7300e60c258b9dce",
+    "url": "/Screen-1-react/static/js/2.4edac465.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Screen-1-react/static/js/2.4edac465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a551498792d0ff9328",
+    "url": "/Screen-1-react/static/js/main.e8e2b53f.chunk.js"
+  },
+  {
+    "revision": "b396cde27d0520d5a823",
+    "url": "/Screen-1-react/static/js/runtime-main.e6ee2c8a.js"
+  }
+]);
